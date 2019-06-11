5f441b209a6e35815453c008cbcd4437ea1019b7
avallecam <avallecam@gmail.com>
2019-06-11 15:08:29 -0500

    first commit

---
+++ R/avallecam.R
@@ -0,0 +1,10 @@
+#' avallecam package
+#'
+#' avallecam - custom functions to solve current problems
+#'
+#' @docType package
+#'
+#' @author Andree Valle Campos \email{avallecam@gmail.com}
+#'
+#' @name avallecam
+NULL
---
+++ R/read_gpx.R
@@ -0,0 +1,43 @@
+#' Read gpx files
+#'
+#' Read gpx files from different GPS tracking devices
+#'
+#' @param list list of gpx files. easier if they came from a list.files() output
+#'
+#' @return dataset of geolocation points with waypoint name and complementary information.
+#'
+#'@export
+
+read_gpx <- function(list) {
+
+  allgpx <- list
+
+  col <- rgdal::readOGR(dsn = allgpx[1], layer="waypoints",verbose = F) %>%
+    as.tibble() %>%
+    select(time,name,desc,contains("coords")) %>%
+    rename("lon"=coords.x1,
+           "lat"=coords.x2) %>%
+    mutate(time=lubridate::ymd_hms(time),
+           id=allgpx[1])
+
+  # loop
+  alltrk <- col[FALSE,]
+  for (i in 1:length(allgpx)) {
+    #gpx.wpt <- rgdal::readOGR(dsn = allgpx[i], layer="waypoints")
+    #gpx.wpt
+    gpx.trk <- rgdal::readOGR(dsn = allgpx[i], layer="waypoints",verbose = F) %>%
+      as.tibble() %>%
+      select(time,name,desc,contains("coords")) %>%
+      rename("lon"=coords.x1,
+             "lat"=coords.x2) %>%
+      mutate(time=lubridate::ymd_hms(time),
+             name=stringr::str_to_lower(name),
+             desc=stringr::str_to_lower(desc),
+             id=allgpx[i])
+
+    alltrk <- alltrk %>% dplyr::union(gpx.trk) %>% arrange(name)
+  }
+
+  return(alltrk)
+
+}
